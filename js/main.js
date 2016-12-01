44a5a8e5b80c124f30093ed48fc7305373e968a3
johngarcia9110 <johngarcia9110@gmail.com>
2016-12-01 11:45:36 -0500

    created example displaying dates with both js and moment to compare most basic implementation

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//create instance of moment object
+var MomentDate = moment();
+
+//display value of momement object on page
+document.getElementById('displayMoment').innerHTML = MomentDate;
+
+//create instance of JS Date object
+var JsDate = new Date();
+
+//display value of JS Date object
+document.getElementById('displayJsDate').innerHTML = JsDate;
