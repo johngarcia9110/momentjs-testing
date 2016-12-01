cb308a7f59640af6a094a645862fa11313283bd0
johngarcia9110 <johngarcia9110@gmail.com>
2016-12-01 11:58:12 -0500

    testing date formating with moment vs date formating with vanilla JS

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
+
+
 //create instance of moment object
 var MomentDate = moment();
 
-//display value of momement object on page
-document.getElementById('displayMoment').innerHTML = MomentDate;
+var momentDateContainer = document.getElementById('displayMoment');
+
+//display value of momement object on page in format D-M-YYYY
+momentDateContainer.innerHTML = MomentDate.format('D-M-YYYY');
+
+/* --------------------------------------------------------------- */
 
 //create instance of JS Date object
 var JsDate = new Date();
 
-//display value of JS Date object
-document.getElementById('displayJsDate').innerHTML = JsDate;
+//setup formating variables
+var jsDateContainer = document.getElementById('displayJsDate');
+var jsDay = JsDate.getDate();
+var jsMonth = JsDate.getMonth() + 1;
+var jsYear = JsDate.getFullYear();
+
+//display new format on page in format D-M-YYYY
+jsDateContainer.innerHTML = jsDay + '-' + jsMonth + '-' + jsYear;
